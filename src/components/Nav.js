630ca0cedb04adea768d1aa853949aae100764a0
Mirza Abdullah Baig <abdullah.baig2013@gmail.com>
2023-11-20 22:14:21 +0100

    navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,35 @@
 import React from "react";
 //import icons
-import { BiHomeAlt, BiUser } from "react-icons/bi";
-import { BsClipboardData, BsBrief, BsChatSquare } from "react-icons/bs";
+import { BiBriefcase, BiClipboard, BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+
+//icons
+import { Link } from "react-scroll";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+      <div className="container mx-auto">
+        <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
+          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BiHomeAlt />
+          </Link>
+
+          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BiUser />
+          </Link>
+
+          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BiClipboard />
+          </Link>
+
+          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BiBriefcase />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
